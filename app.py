ff7d0f0247b2223ad9c00838f500c54359f30bb4
jasperchan <jscnn51011@gmail.com>
2021-09-15 00:43:28 +0800

    Add code

--- app.py
+++ app.py
@@ -34,7 +34,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    if (message == "野"):
+        line_bot_api.reply_message(event.reply_token, "格")
+    
 
 import os
 if __name__ == "__main__":
