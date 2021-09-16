8e282ffe01993faccaba23756dba7806ed7d3fd7
jasperchan <jscnn51011@gmail.com>
2021-09-17 05:49:40 +0800

    Add code

--- crawler.py
+++ crawler.py
@@ -4,12 +4,31 @@ from bs4 import BeautifulSoup
 class nHentaiSearcher:
     def __init__(self,num):
         self.__target = requests.get("https://nhentai.net/g/"+num)
+        self.__num = num
+        self.__tempStr = ""
     def searchTitle(self):
-        page = BeautifulSoup(self.__target.text,"html.parser")
-        title = [page.find("span","before").text,page.find("span","pretty").text,page.find("span","after").text]
-        tempStr = ""
+        if self.__target.status_code != 200:
+            return self.__num + "哭啊，查無此本"
+        else: 
+            page = BeautifulSoup(self.__target.text,"html.parser")
+            title = [page.find("span","before").text,page.find("span","pretty").text,page.find("span","after").text]
+            tempTag = page.find_all("span","name")
+            tagCnt = 0
+            tags = []
+            for i in tempTag:
+                tags += tempTag[tagCnt].text
+                tags += " "
+                tagCnt += 1
+            return self.processInfo(title,tags)
+    def processInfo(self,title,tags):
+        self.__tempStr += self.__num
+        self.__tempStr += "\n"
+        tagCnt = 0
         for i in range(0,3):
-            tempStr+=title[i]
-        return tempStr
-    def getPage(self,num):
-        return "https://nhentai.net/g/"+num
+            self.__tempStr += title[i]
+        for i in tags:
+            self.__tempStr += tags[tagCnt]
+            tagCnt += 1
+        return self.__tempStr
+    def getPage(self):
+        return "https://nhentai.net/g/"+self.__num
