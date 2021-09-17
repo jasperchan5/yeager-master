fea5408f1dc794b0cc65b03a731935e966f9a78d
jasperchan <jscnn51011@gmail.com>
2021-09-17 19:59:23 +0800

    Add code

--- crawler.py
+++ crawler.py
@@ -31,3 +31,38 @@ class nHentaiSearcher:
             self.__tempStr += tags[tagCnt]
             tagCnt += 1
         return self.__tempStr
+
+class pixivSearcher:
+    def __init__(self,num):
+        self.__target = requests.get("https://www.pixiv.net/artworks/"+num)
+        self.__num = num
+        self.__tempStr = ""
+    def searchTitle(self):
+        if self.__target.status_code != 200:
+            return self.__num + "\n哭啊，查無此作品"
+        else: 
+            page = BeautifulSoup(self.__target.text,"html.parser")
+            title = page.find("title").text
+            tempTag = page.find_all("title")
+            tagCnt = 0
+            tags = ["\n"]
+            for i in tempTag:
+                print(tempTag[tagCnt].text)
+                tags += "[" + tempTag[tagCnt].text + "]"
+                tags += " "
+                tagCnt += 1
+                if tagCnt % 3 == 0:
+                    tags += "\n"
+            return self.processInfo(title,tags)
+    def processInfo(self,title,tags):
+        self.__tempStr += self.__num
+        self.__tempStr += "\n\n"
+        tagCnt = 0
+        self.__tempStr += title + "\n"
+        for i in tags:
+            self.__tempStr += tags[tagCnt]
+            tagCnt += 1
+        return self.__tempStr
+    
+# a = pixivSearcher(input())
+# print(a.searchTitle())
