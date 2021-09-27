882b237f43988e875bf4982433bf2ab49aa1331c
jasperchan <jscnn51011@gmail.com>
2021-09-28 01:08:05 +0800

    TicTacToe

--- app.py
+++ app.py
@@ -14,8 +14,12 @@ import random as rd
 
 from solitaire import Solitaire
 from crawler import nHentaiSearcher, imageSearcher, tagSearcher, covid19
+from ticTacToe import TicTacToe
 
 solitaireList = []
+TicTacToeMode = False
+TicTacToeStarted = False
+
 
 app = Flask(__name__)
 
@@ -43,73 +47,86 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
+    global TicTacToeMode
     message = event.message.text
-    if message == '指令':
-        orders = '很高興認識你，我是接龍大師。\n\n【 功能列表 】\n\n─〔接龍〕─\n野格炸彈\n星爆\n田勝傑\n南一中蜜蜂\n我難過\n\n─〔推本子〕─\n隨機推本：請輸入「神之語言」\n本號查詢：請輸入「神之語言 <任意數字>」\n標籤查詢：請輸入「找本子 <tag1> <tag2>...」\n\n─〔推圖〕─\nR-18：請輸入「可以色色」（開發中）\n正常向：請輸入「不可以色色」（開發中）\n\n─〔每日疫情資訊〕─\n請輸入「疫情報告」\n\n更多功能敬請期待...'
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=orders))
-    elif message == '神之語言':
-        randNum = rd.randint(0,400000)
-        hentaiSearch = nHentaiSearcher(str(randNum))
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=hentaiSearch.searchTitle()))
-    elif '神之語言 ' in message:
-        doujinNum = message.split(" ")[1]
-        hentaiSearch = nHentaiSearcher(doujinNum)
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=hentaiSearch.searchTitle()))
-    elif '找本子 ' in message:
-        temp = message.split(" ")
-        tag = ""
-        tagCnt = 0
-        for i in temp:
-            if tagCnt != 0:
-                tag += temp[tagCnt]
-                if tagCnt < len(temp)-1:
-                    tag += '+'
+    if TicTacToeMode == False:
+        if message == '指令':
+            orders = '很高興認識你，我是接龍大師。\n\n【 功能列表 】\n\n─〔接龍〕─\n野格炸彈\n星爆\n田勝傑\n南一中蜜蜂\n我難過\n\n輸入「接龍進度」以查看進行中的接龍\n\n─〔井字遊戲〕─\n請輸入「井字遊戲」切換\n請輸入「別玩了」終止遊戲\n\n─〔推本子〕─\n隨機推本：請輸入「神之語言」\n本號查詢：請輸入「神之語言 <任意數字>」\n標籤查詢：請輸入「找本子 <tag1> <tag2>...」\n\n─〔推圖〕─\nR-18：請輸入「可以色色」（開發中）\n正常向：請輸入「不可以色色」（開發中）\n\n─〔每日疫情資訊〕─\n請輸入「疫情報告」\n\n更多功能敬請期待...'
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=orders))
+        elif message == '井字遊戲':
+            TicTacToeMode = True
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Switch！！！\n\n【井字遊戲模式】\n\n─〔規則說明〕─\n首先輸入英文大寫O或X來選擇自己的符號\n接著就輸入<座標一 座標二>來決定劃記地點\n棋盤為5x5，向下為X座標，向右為Y座標"))
+        elif message == '神之語言':
+            randNum = rd.randint(0,400000)
+            hentaiSearch = nHentaiSearcher(str(randNum))
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=hentaiSearch.searchTitle()))
+        elif '神之語言 ' in message:
+            doujinNum = message.split(" ")[1]
+            hentaiSearch = nHentaiSearcher(doujinNum)
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=hentaiSearch.searchTitle()))
+        elif '找本子 ' in message:
+            temp = message.split(" ")
+            tag = ""
+            tagCnt = 0
+            for i in temp:
+                if tagCnt != 0:
+                    tag += temp[tagCnt]
+                    if tagCnt < len(temp)-1:
+                        tag += '+'
+                        tagCnt += 1
+                else:
                     tagCnt += 1
+            doujinSearch = tagSearcher(tag)
+            try:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text=doujinSearch.searchDoujin()))
+            except:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text="哭啊，找本失敗！"))
+        elif message == '不可以色色' or message == '可以色色':
+            imgBot = imageSearcher(message)
+            imgLink = imgBot.getImage()
+            try:
+                line_bot_api.reply_message(event.reply_token, ImageSendMessage(
+                                                                original_content_url = imgLink,
+                                                                preview_image_url = imgLink))
+            except:
+                line_bot_api.reply_message(event.reply_token, ImageSendMessage(
+                                                                original_content_url = 'https://imgur.com/mj4CCdA.png',
+                                                                preview_image_url = 'https://imgur.com/mj4CCdA.png'))
+        elif message == '疫情報告':
+            covidBot = covid19()
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=covidBot.getDailyInfo()))
+        elif message == '接龍進度':
+            if len(solitaireList) == 0:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text="無接龍進行中"))
             else:
-                tagCnt += 1
-        doujinSearch = tagSearcher(tag)
-        try:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=doujinSearch.searchDoujin()))
-        except:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="哭啊，找本失敗！"))
-    elif message == '不可以色色' or message == '可以色色':
-        imgBot = imageSearcher(message)
-        imgLink = imgBot.getImage()
-        try:
-            line_bot_api.reply_message(event.reply_token, ImageSendMessage(
-                                                            original_content_url = imgLink,
-                                                            preview_image_url = imgLink))
-        except:
-            line_bot_api.reply_message(event.reply_token, ImageSendMessage(
-                                                            original_content_url = 'https://imgur.com/mj4CCdA.png',
-                                                            preview_image_url = 'https://imgur.com/mj4CCdA.png'))
-    elif message == '疫情報告':
-        covidBot = covid19()
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=covidBot.getDailyInfo()))
-    elif message == '接龍進度':
-        if len(solitaireList) == 0:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="無接龍進行中"))
+                cnt = 0
+                tempStr = ""
+                for i in solitaireList:
+                    tempStr += solitaireList[cnt]
+                    cnt += 1
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text=tempStr))
         else:
-            cnt = 0
-            tempStr = ""
-            for i in solitaireList:
-                tempStr += solitaireList[cnt]
-                cnt += 1
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=tempStr))
+            soliModel = Solitaire(message,solitaireList)
+            answer = soliModel.processer()
+            if len(solitaireList) != 0 and solitaireList[len(solitaireList) - 1] == '秒':
+                solitaireList.clear()
+                replyArr = []
+                replyArr.append(TextSendMessage(text="秒"))
+                replyArr.append(ImageSendMessage(
+                                                original_content_url = 'https://imgur.com/2CWEKvS.png',
+                                                preview_image_url = 'https://imgur.com/2CWEKvS.png'))
+                line_bot_api.reply_message(event.reply_token, replyArr)
+            elif len(solitaireList) >= 0:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
     else:
-        soliModel = Solitaire(message,solitaireList)
-        answer = soliModel.processer()
-        if len(solitaireList) != 0 and solitaireList[len(solitaireList) - 1] == '秒':
-            solitaireList.clear()
-            replyArr = []
-            replyArr.append(TextSendMessage(text="秒"))
-            replyArr.append(ImageSendMessage(
-                                            original_content_url = 'https://imgur.com/2CWEKvS.png',
-                                            preview_image_url = 'https://imgur.com/2CWEKvS.png'))
-            line_bot_api.reply_message(event.reply_token, replyArr)
-        elif len(solitaireList) >= 0:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
-
+        if message == "別玩了":
+            TicTacToeMode = False
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="雖然是遊戲，但可不是鬧著玩的！"))
+        else:
+            if message == "O" or message == "X":
+                message = message.split(" ")
+                
+            
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
---
+++ ticTacToe.py
@@ -0,0 +1,124 @@
+import numpy as np
+import random as rd
+playerInfo = []
+botInfo = []
+nowCourt = ""
+class TicTacToe:
+    def __init__(self,select,started):
+
+        if started == False:
+            # Initiate the game
+            self.court = np.empty([3,3], dtype = object)
+            for i in range(0,3):
+                for j in range(0,3):
+                    self.court[i][j] = "_"
+            self.originalCourt = self.court
+
+            # Select player identity
+            self.player = [False,""]
+            self.bot = [False,""]
+            if self.player[0] == False and self.bot[0] == False:
+                if select == "O" :
+                    self.player = [True,"O"]
+                    self.bot = [True,"X"]
+                elif select == "X":
+                    self.player = [True,"X"]
+                    self.bot = [True,"O"]
+
+            global playerInfo, botInfo
+            playerInfo = self.player
+            botInfo = self.bot
+
+        else:
+            # Load records
+            for i in range(0,3):
+                for j in range(0,3):
+                    self.court = nowCourt[i][j]
+            
+
+    def recordCourt(self):
+        tempStr = ""
+        for i in range(0,3):
+            for j in range(0,3):
+                tempStr += self.court[i][j]
+            tempStr += "\n"
+        nowCourt = tempStr
+        return nowCourt
+
+    def displayPlayer(self,coordX,coordY):
+        if self.court[int(coordX)][int(coordY)]  == "_": 
+            self.court[int(coordX)][int(coordY)] = self.player[1]
+        else:
+            if self.court[int(coordX)][int(coordY)]  == "X": 
+                print("還敢蓋別人啊，bad。")
+            else:
+                print("這個位置下過了，北七。")
+        self.recordCourt()
+
+    def displayBot(self):
+        posX = rd.randint(0,2)
+        posY = rd.randint(0,2)
+        if self.court[posX][posY] == "_":
+            self.court[posX][posY] = self.bot[1]
+            self.recordCourt()
+        else:
+            # print("collided")
+            self.displayBot()
+
+    def clear(self):
+        self.court = self.originalCourt
+
+    def endGame(self):
+        # Case row or column
+        for i in range(0,3):
+            occupiedO = 0
+            occupiedX = 0
+            for j in range(0,3):
+                if self.court[i][j] == "O":
+                    occupiedO += 1
+                    if occupiedO == 3:
+                        print("You win")
+                        return True
+                elif self.court[i][j] == "X":
+                    occupiedX += 1
+                    if occupiedX == 3:
+                        return True
+        for i in range(0,3):
+            occupiedO = 0
+            occupiedX = 0
+            for j in range(0,3):
+                if self.court[i][j] == "O":
+                    occupiedO += 1
+                    if occupiedO == 3:
+                        print("You win")
+                        return True
+                elif self.court[i][j] == "X":
+                    occupiedX += 1
+                    if occupiedX == 3:
+                        return True
+        # Case slant
+        occupiedO = 0
+        occupiedX = 0
+        for i in range(0,3):
+            for j in range(0,3):
+                if i == j:
+                    if self.court[i][j] == "O":
+                        occupiedO += 1
+                        if occupiedO == 3:
+                            print("You win")
+                            return True
+                    elif self.court[i][j] == "X":
+                        occupiedX += 1
+                        if occupiedX == 3:
+                            return True
+        return False
+
+a = TicTacToe("O",nowCourt="",started=False)
+print(playerInfo,botInfo)
+while(a.endGame() == False):
+    b = input()
+    b = b.split(" ")
+    a.displayPlayer(b[0],b[1])
+    if a.endGame() == True: break
+    a.displayBot()
+    if a.endGame() == True: break
